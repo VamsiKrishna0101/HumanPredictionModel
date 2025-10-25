19eaa0438ead2f0bec0850a086786c5ef6008f7c
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-10-25 18:08:29 +0530

    Update config.py

--- HumanDirectionModel/app/core/config.py
+++ HumanDirectionModel/app/core/config.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Settings:
-    POSE_MODEL: str = "yolov8n-pose.pt"
+    POSE_MODEL: str = "yolov8n-pose-tiny.pt"
     PERSON_CONF_THR: float = 0.35
     KEYPOINT_CONF_THR: float = 0.25
     FULLBODY_MIN_VISIBLE_KPTS: int = 12
